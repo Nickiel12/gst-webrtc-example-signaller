ba3ad5ad2472d117789f2e7008f1702f5d84b9ff
Nickiel12 <35903114+Nickiel12@users.noreply.github.com>
2024-08-08 09:24:52 -0700

    added gstrswebrtc plugin register code

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,14 @@ use gstrswebrtc::webrtcsink;
 fn main() -> Result<(), Error> {
     gstreamer::init()?;
 
+    // this call registers the webRTC plugin
+    // in-place of the dynamic library.
+    // if you run this code and it throws an error that GstSignaller is already registered,
+    // make sure that gstreamer cannot load the system library for 
+    // gst-plugins-rs, as it is registering that name
+    // but the rust module tries to register itself when the class is referenced
+    gstrswebrtc::plugin_register_static()?;
+
     let custom_signaller = signaller::MyCustomSignaller::new();
     let webrtcsink = webrtcsink::BaseWebRTCSink::with_signaller(
         signaller_interface::Signallable::from(custom_signaller),
